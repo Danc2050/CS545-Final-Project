bb7b8df2dc7b6c0e3460c0ed4a961a68c1335db7
Daniel Connelly <Danc2@pdx.edu>
2019-11-19 22:20:27 -0800

    Create neural.py

---
+++ neural.py
@@ -0,0 +1,11 @@
+'''
+Our neural net implementation on the housing data.
+Details:
+1 Hidden layer (due to UAT), Dimensionality reduction.
+Dalton, Ebele, Dawei, Daniel Lee, Daniel Connelly
+'''
+def main():
+  pass
+  
+if __name__=="__main__":
+  main()
