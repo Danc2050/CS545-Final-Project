a895fc628a2b6133094ea4e1fb9ad6fe3699ede5
Daniel Connelly <Danc2@pdx.edu>
2019-11-19 22:21:29 -0800

    Create bayes.py

---
+++ bayes.py
@@ -0,0 +1,11 @@
+'''
+Our Gaussian Naive Bayes implementation on the housing data.
+Details: Dimensionality reduction.
+Authors: Dalton, Ebele, Dawei, Daniel Lee, Daniel Connelly
+Class: CS545 - Fall 2019 | Professor Anthony Rhodes
+'''
+def main():
+  pass
+  
+if __name__=="__main__":
+  main()
