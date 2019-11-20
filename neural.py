4715025626a43d398fa5f8deaf6a8b2e9ed0344a
Daniel Connelly <Danc2@pdx.edu>
2019-11-19 22:21:52 -0800

    Update neural.py

--- neural.py
+++ neural.py
@@ -1,8 +1,8 @@
 '''
 Our neural net implementation on the housing data.
-Details:
-1 Hidden layer (due to UAT), Dimensionality reduction.
-Dalton, Ebele, Dawei, Daniel Lee, Daniel Connelly
+Details: 1 Hidden layer (due to UAT), Dimensionality reduction.
+Authors: Dalton, Ebele, Dawei, Daniel Lee, Daniel Connelly
+Class: CS545 - Fall 2019 | Professor Anthony Rhodes
 '''
 def main():
   pass
