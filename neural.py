410ac65bccb1dda45fbd4a87effcda38f72c3a16
ddveloper <44758313+ddveloper@users.noreply.github.com>
2019-11-28 13:17:14 -0800

    rearrange data before splitting, so labels distribute evenly

--- neural.py
+++ neural.py
@@ -19,11 +19,36 @@ def normalize_data(data):
   ret = ret / np.transpose(stds).reshape(1, dim)
   return ret
 
+def alter_labels(data, idx_label):
+  ''' in-place rearrange data so labels altering evenly
+      in: np array with all raw dataset
+      out: none
+  '''
+  labels = data[:, [idx_label]]
+  unique, counts = np.unique(labels, return_counts=True)
+  cnt_labels = dict(zip(unique, counts))
+  # labels 0:1 = 23364:6636 = 3.52:1 ~= 3.5:1 = 7:2
+  p1, p2 = 0, 0
+  length = np.shape(data)[0]
+  while p1 < length and p2 < length:
+    target = 1 if p1%9 == 3 or p1%9 == 8 else 0
+    if data[p1, idx_label] != target:
+      p2 = p1 + 1
+      while p2 < length and data[p2, idx_label] != target:
+        p2 += 1
+      if p2 < length and data[p2, idx_label] == target:
+        data[[p1, p2]] = data[[p2, p1]] # swap them
+    p1 += 1
+  labels = data[:, [idx_label]]
+  # move dirty distrubution top, so training set will use them
+  np.flip(data, axis=0)
+
 def prepare_data(data, idx_label):
   ''' preprocess data for training
       in: np array with all raw dataset
       out: inputs, labels as np arrays
   '''
+  alter_labels(data, idx_label) # rearrange data for later splitting
   labels = data[:, [idx_label]]
   raw_examples = np.delete(data, idx_label, axis=1)
   examples = normalize_data(raw_examples)
