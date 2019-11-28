7e3a442b69ba9194bdeafa9e76d09b842b9ddce9
daltonbohning <daltonbohning@gmail.com>
2019-11-27 18:11:45 -0800

    Run each version in a separate process and output to file.

--- run.py
+++ run.py
@@ -8,21 +8,25 @@
 import sys
 import getopt
 import subprocess
+import multiprocessing as mp
 
 
 assert sys.version_info >= (3, 6) #Pyhon 3 required
 
 quiet = False
+serial = False
+
 
 def main(argv):
   global quiet
+  global serial
   versions = []
   def usage():
-    print(f'Usage: {argv[0]} [--quiet] [-v <bayes | slp | mlp>]')
+    print(f'Usage: {argv[0]} [--quiet] [--serial] [-v <bayes | slp | mlp>]')
     exit(2)
 
   try: 
-    opts, args = getopt.getopt(argv[1:], ":v:q", ['quiet'])
+    opts, args = getopt.getopt(argv[1:], ":v:qs", ['quiet', 'serial'])
   except getopt.GetoptError: 
     usage()
 
@@ -36,31 +40,61 @@ def main(argv):
       versions.append(arg)
     if opt in ('-q', '--quiet'):
       quiet = True
+    if opt in ('-s', '--serial'):
+      serial = True
 
   versions = list(set(versions)) #Make distinct
   if versions == []: versions = ['bayes', 'slp', 'mlp'] #Run them all
 
   installModules()
-  if 'bayes' in versions: bayes()
-  if 'slp' in versions: slp()
-  if 'mlp' in versions: mlp()
+
+  processes = []
+  if 'bayes' in versions:
+    processes.append(mp.Process(target=bayes))
+  if 'slp' in versions: 
+    processes.append(mp.Process(target=slp))
+  if 'mlp' in versions: 
+    processes.append(mp.Process(target=mlp))
+
+
+  if serial:
+    for p in processes:
+      p.start()
+      p.join()
+  else:
+    for p in processes:
+      p.start()
+    for p in processes:
+      p.join()
 
 
 def bayes():
-  output('\n\nExecuting bayes...')
-  import bayes
-  bayes.main()
-
+  outfilename = '_bayes.txt'
+  output(f'Executing bayes (output in {outfilename})')
+  with open(outfilename, 'w+') as outfile:
+    sys.stdout = sys.stderr = outfile
+    import bayes
+    bayes.main()
+    
 def slp():
-  output('\n\nExecuting slp...')
-  print('TODO')
+  outfilename = '_slp.txt'
+  output(f'Executing slp (output in {outfilename})')
+  with open(outfilename, 'w+') as outfile:
+    sys.stdout = sys.stderr = outfile
+    print('TODO')
 
 def mlp():
-  output('\n\nExecuting mlp...')
-  import neural
-  neural.main()
+  outfilename = '_mlp.txt'
+  output(f'Executing mlp (output in {outfilename})')
+  with open(outfilename, 'w+') as outfile:
+    sys.stdout = sys.stderr = outfile
+    import neural
+    neural.main()
 
 
+'''
+  Makes sure we have the necessary modules installed.
+'''
 def installModules():
   output('Checking for required modules... ', end='')
 
@@ -73,6 +107,9 @@ def installModules():
   output('Done')
 
 
+'''
+  Allows output to be easily turned on or off.
+'''
 def output(s, end='\n'):
   if not quiet: print(s, end=end, flush=True)
 
