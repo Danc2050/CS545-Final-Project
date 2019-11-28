029cd6c2cc8d8bb1e171175a949ab99a3cf0e99a
daltonbohning <daltonbohning@gmail.com>
2019-11-27 17:23:14 -0800

    Rudimentary function to run all models.

---
+++ run.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+
+'''
+  Authors: Dalton, Ebele, Dawei, Daniel Lee, Daniel Connelly
+  Class: CS545 - Fall 2019 | Professor Anthony Rhodes
+'''
+
+import sys
+import getopt
+import subprocess
+
+
+assert sys.version_info >= (3, 6) #Pyhon 3 required
+
+
+def main(argv):
+  installModules()
+
+  versions = []
+  def usage():
+    print(f'Usage: {argv[0]} [-v <bayes | slp | mlp>]')
+    exit(2)
+
+  try: 
+    opts, args = getopt.getopt(argv[1:], ":v:")
+  except getopt.GetoptError: 
+    usage()
+
+  #Reject extraneous arguments
+  if len(args) > 0: usage()
+
+  for opt, arg in opts:
+    if opt in ('-v', '-version'):
+      if arg not in ('bayes', 'slp', 'mlp'):
+        usage()
+      versions.append(arg)
+
+  versions = list(set(versions)) #Make distinct
+  if versions == []: versions = ['bayes', 'slp', 'mlp'] #Run them all
+
+  if 'bayes' in versions:
+    bayes()
+  if 'slp' in versions:
+    slp()
+  if 'mlp' in versions:
+    mlp()
+
+
+def bayes():
+  print('\n\nExecuting bayes...')
+  import bayes
+  bayes.main()
+
+def slp():
+  print('\n\nExecuting slp...')
+  print('TODO')
+
+def mlp():
+  print('\n\nExecuting mlp...')
+  import neural
+  neural.main()
+
+
+def installModules():
+  print('Checking for required modules... ', end='', flush=True)
+
+  try:
+    import sklearn
+  except ImportError as error:
+    print('sklearn not found. Installing...')
+    p = subprocess.run('pip3 install sklearn', shell=True)
+    
+  print('Done')
+
+
+if __name__=="__main__":
+  main(sys.argv)
