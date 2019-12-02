93b77b63bd74a9383be65895e645b159f456cac0
Dalton Bohning <daltonbohning@gmail.com>
2019-12-02 10:14:19 -0800

    Removed confusion matrixx from slp_weight_update because it should not be there

--- SLP/slp.py
+++ SLP/slp.py
@@ -39,14 +39,13 @@ def confusion_matrix_create(number_of_outputs):
 	conf_matrix = np.zeros((number_of_outputs,number_of_outputs),dtype=float)
 	return conf_matrix;
 
-def slp_weight_update(data_input,weights,label_input,learning_rate,confusion_matrix):
+def slp_weight_update(data_input,weights,label_input,learning_rate):
 	"""
 	computes the output from using the input and weights by using a dot product
 	data_input: A row from the data set
 	weights: The weight matrix for the current layer
 	label_input: The label for the row
 	learning_rate: The learning rate from gradient descent formula
-	confusion_matrix: Keeps track of target outputs versus actual outputs
 	"""
 
 	output = np.dot(data_input,np.transpose(weights)) #transpose to allow for dot product
@@ -54,18 +53,6 @@ def slp_weight_update(data_input,weights,label_input,learning_rate,confusion_mat
 		output = 1
 	else:
 		output = 0
-	#confusion matrix portion
-	if (output == 1):
-		if (label_input == 1):
-			confusion_matrix[1][1] += 1
-		else:
-			confusion_matrix[0][1] += 1
-	else:
-		if (label_input == 0):
-			confusion_matrix[0][0] += 1
-		else:
-			confusion_matrix[1][0] += 1
-	#weight update portion
 	if (label_input != output): #the target is not the same change weights
 		for x in range(weights.shape[0]): #goes through all the weights
 			weights[x] = weights[x] - (learning_rate * (output - label_input) * data_input[x])
@@ -82,7 +69,7 @@ def epoch_train_slp(data_input,label_input, learning_rate,weights, confusion_mat
 	for epoch in range(epoch_input):
 		np.random.shuffle(training_pointer)# randomize the array
 		for x in range(label_input.shape[0]):
-			weights = slp_weight_update(data_input[(training_pointer[x]),:],weights,label_input[(training_pointer[x])],learning_rate, confusion_matrix)
+			weights = slp_weight_update(data_input[(training_pointer[x]),:],weights,label_input[(training_pointer[x])],learning_rate)
 		#throw back in the train data
 		np.random.shuffle(training_pointer) # randomize
 		for xt in range(label_input.shape[0]):
@@ -105,7 +92,7 @@ def test_slp(weights,confusion_matrix,label_tests,input_tests):
 	input_tests: test data
 	label_tests: labels for the test data
 	"""
-	output = np.dot(input_tests,np.transpose(weights)) #transpose to allow for dot product
+	output = np.dot(input_tests, weights.T) #transpose to allow for dot product
 	output = 1 if output > 0 else 0
 	confusion_matrix[int(label_tests)][output] += 1
 
