913ec1b0e695843807fc655fdc1e1f812ca4bab1
Dalton Bohning <daltonbohning@gmail.com>
2019-12-07 17:21:29 -0800

    Replaced functions with skleanr.model_selection.train_test_split

--- prepare.py
+++ prepare.py
@@ -5,6 +5,8 @@ Class: CS545 - Fall 2019 | Professor Anthony Rhodes
 '''
 import numpy as np
 from os import path
+from sklearn.model_selection import train_test_split
+
 
 def normalize_data(data):
   ''' normalize data for training
@@ -20,56 +22,22 @@ def normalize_data(data):
   ret = ret / np.transpose(stds).reshape(1, dim)
   return ret
 
-def alter_labels(data, idx_label):
-  ''' in-place rearrange data so labels altering evenly
-      in: np array with all raw dataset
-      out: none
-  '''
-  labels = data[:, [idx_label]]
-  unique, counts = np.unique(labels, return_counts=True)
-  cnt_labels = dict(zip(unique, counts))
-  # labels 0:1 = 23364:6636 = 3.52:1 ~= 3.5:1 = 7:2
-  # Warning: below code is hard coded for 7:2 case
-  p1, p2 = 0, 0
-  length = np.shape(data)[0]
-  while p1 < length and p2 < length:
-    target = 1 if p1%9 == 3 or p1%9 == 8 else 0
-    if data[p1, idx_label] != target:
-      p2 = p1 + 1
-      while p2 < length and data[p2, idx_label] != target:
-        p2 += 1
-      if p2 < length and data[p2, idx_label] == target:
-        data[[p1, p2]] = data[[p2, p1]] # swap them
-    p1 += 1
-  labels = data[:, [idx_label]]
-
-def split_indexes(n_train, n_test, row_total):
-  ''' calculate where to split
-  '''
-  i0 = n_train / (n_train + n_test) * row_total
-  i1 = (n_train) / (n_train + n_test) * row_total
-  return (int(i0), int(i1))
 
 def prepare_data(data, idx_label, train_n, test_n):
   ''' preprocess data for training
       in: np array with all raw dataset
       out: inputs, labels as np arrays
   '''
-  alter_labels(data, idx_label) # rearrange data for later splitting
-
-  labels = data[:, [idx_label]] # grab labels
+  labels = data[:, [idx_label]] #Get labels column
   labels = labels - np.amin(labels) # remove offset, so start from '0'
   n_class = int(np.amax(labels) + 1) # assume continous class numbers
+  data = np.delete(data, idx_label, axis=1) #Remove labels from data
+  data = normalize_data(data)
+  data_train, data_test, labels_train, labels_test = train_test_split(data, labels, test_size=0.25)
+  data_train = (data_train, labels_train)
+  data_test = (data_test, labels_test)
+  return (data_train, data_test, n_class)
 
-  raw_examples = np.delete(data, idx_label, axis=1) # grab examples
-  examples = normalize_data(raw_examples) # normalize
-
-  # split
-  (i0, i1) = split_indexes(train_n, test_n, np.shape(examples)[0])
-  train_set = (examples[:i0], labels[0:i0])
-  test_set = (examples[i1:], labels[i1:])
-
-  return (train_set, test_set, n_class)
   
 if __name__=="__main__":
   data = np.genfromtxt(path.join('data', 'data.csv'), delimiter=',')
